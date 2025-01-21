e3e785d55252f49d8fa275b3eb5b442933c59695
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-01-21 06:57:32 +0530

    Commit

--- src/lecture_3_Recursion_1/assignment/Sum_of_digits_recursive.java
+++ src/lecture_3_Recursion_1/assignment/Sum_of_digits_recursive.java
@@ -1,5 +1,22 @@
 package lecture_3_Recursion_1.assignment;
 
+import java.util.Scanner;
+
 public class Sum_of_digits_recursive {
+	
+public static int sumOfDigits(int input){
+		
+		if(input == 0){
+			return 0;
+		}
+
+		return (input%10) + sumOfDigits(input/10);
+	}
+
+	public static void main(String[] args) {
+	Scanner s = new Scanner(System.in);
+	int n = s.nextInt();
+	System.out.println(sumOfDigits(n));
+}
 
 }
