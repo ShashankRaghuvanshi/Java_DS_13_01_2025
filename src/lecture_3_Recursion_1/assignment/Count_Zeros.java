2df6545f5ef8c7724ef043c6f94e1f7e6e34cbcb
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2025-02-17 10:07:36 +0530

    Commit

--- src/lecture_3_Recursion_1/assignment/Count_Zeros.java
+++ src/lecture_3_Recursion_1/assignment/Count_Zeros.java
@@ -31,7 +31,7 @@ public class Count_Zeros {
     public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
 		int n = s.nextInt();
-		System.out.println(countZerosRec(n));
+		System.out.println(countZerosRec(n)); 
 	}
 
 }
